90b6d4843ae0157132404b615abee87f2eda3aad
Oliulla <razasm29@gmail.com>
2023-07-31 22:39:12 +0600

    [DONE]: Auth for users

--- src/app/modules/myProfile/myProfile.route.ts
+++ src/app/modules/myProfile/myProfile.route.ts
@@ -1,12 +1,30 @@
 import express from "express"
 import { userProfileController } from "./myProfile.controller"
+import auth from "../../middlewares/auth"
+import { ENUM_USER_ROLE } from "../../../enums/user"
 // import { userProfileController } from "./user.controller"
 
 const router = express.Router()
 
-router.get("/:id", userProfileController.getSingleUserProfile)
-router.patch("/:id", userProfileController.updateUserProfile)
-router.delete("/:id", userProfileController.deleteUserProfile)
-router.get("/", userProfileController.getUserProfiles)
+router.get(
+  "/:id",
+  auth(ENUM_USER_ROLE.ADMIN),
+  userProfileController.getSingleUserProfile
+)
+router.patch(
+  "/:id",
+  auth(ENUM_USER_ROLE.ADMIN),
+  userProfileController.updateUserProfile
+)
+router.delete(
+  "/:id",
+  auth(ENUM_USER_ROLE.ADMIN),
+  userProfileController.deleteUserProfile
+)
+router.get(
+  "/",
+  auth(ENUM_USER_ROLE.ADMIN),
+  userProfileController.getUserProfiles
+)
 
 export const userProfileRoutes = router
