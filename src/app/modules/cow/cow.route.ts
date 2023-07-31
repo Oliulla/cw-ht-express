47bb3c25c31a18b165f35af15de56e4f4c58c1f6
Oliulla <razasm29@gmail.com>
2023-07-31 22:50:00 +0600

    [ADDED]: Auth for cow

--- src/app/modules/cow/cow.route.ts
+++ src/app/modules/cow/cow.route.ts
@@ -1,13 +1,23 @@
 import express from "express"
 import { cowController } from "./cow.controller"
+import auth from "../../middlewares/auth"
+import { ENUM_USER_ROLE } from "../../../enums/user"
 
 const router = express.Router()
 
 // Define your routes here
-router.post("/", cowController.createCow)
-router.get("/", cowController.getAllCows)
-router.get("/:id", cowController.getCowById)
-router.patch("/:id", cowController.updateCowById)
-router.delete("/:id", cowController.deleteCowById)
+router.post("/", auth(ENUM_USER_ROLE.SELLER), cowController.createCow)
+router.get(
+  "/",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER),
+  cowController.getAllCows
+)
+router.get(
+  "/:id",
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER),
+  cowController.getCowById
+)
+router.patch("/:id", auth(ENUM_USER_ROLE.SELLER), cowController.updateCowById)
+router.delete("/:id", auth(ENUM_USER_ROLE.SELLER), cowController.deleteCowById)
 
 export const cowRoutes = router
